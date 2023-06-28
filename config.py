0c3f85023606d6b05cddf4946a99cc35cfe127a6
Federico6419 <102321945+Federico6419@users.noreply.github.com>
2023-06-28 20:38:27 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,22 +2,24 @@ import torch
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
-DEVICE ="cuda" if torch.cuda.is_available() else "cpu"
-TRAIN_DIR = "Datasets/Train"
-TEST_DIR = "Datasets/Test"
+DEVICE ="cuda" if torch.cuda.is_available() else "cpu"              #Use cuda as device if available, else use the gpu
+TRAIN_DIR = "Datasets/Train"                                        #Train Datasets Directory
+TEST_DIR = "Datasets/Test"                                          #Test Datasets Directory
 
-LEARNING_RATE = 2e-5
+LEARNING_RATE = 2e-5                                            
 LAMBDA_CYCLE = 10
-LAMBDA_IDENTITY = 5 # 0.5*lambda_cycle
+LAMBDA_IDENTITY = 5
 LAMBDA_MASK = 0.7
 LAMBDA_CYCLE_MASK = 0.3
 NUM_EPOCHS = 10
 LOAD_MODEL = False
 SAVE_MODEL = True
 TRAIN_MODEL = True
-TRANSFORMATION = "HorseToZebra"                          
+TRANSFORMATION = "HorseToZebra"       
+TRAIN_MASK = "Random"
 TEST_MASK = "Random"
 
+#Checkpoints to load
 CHECKPOINT_GEN_A = "../drive/MyDrive/Checkpoints/HorseToZebra20/gen_a.pth.tar"     
 CHECKPOINT_GEN_B= "../drive/MyDrive/Checkpoints/HorseToZebra20/gen_b.pth.tar"
 CHECKPOINT_DISC_A = "../drive/MyDrive/Checkpoints/HorseToZebra20/disc_a.pth.tar"
@@ -25,6 +27,7 @@ CHECKPOINT_DISC_B = "../drive/MyDrive/Checkpoints/AppleToOrange40/disc_b.pth.tar
 CHECKPOINT_DISC_AM = "../drive/MyDrive/Checkpoints/AppleToOrange40/disc_am.pth.tar"
 CHECKPOINT_DISC_BM = "../drive/MyDrive/Checkpoints/AppleToOrange40/disc_bm.pth.tar"
 
+#Checkpoints to save
 NEW_CHECKPOINT_GEN_A = "../drive/MyDrive/Checkpoints/HorseToZebra20No2/gen_a.pth.tar"     
 NEW_CHECKPOINT_GEN_B= "../drive/MyDrive/Checkpoints/HorseToZebra20No2/gen_b.pth.tar"
 NEW_CHECKPOINT_DISC_A = "../drive/MyDrive/Checkpoints/HorseToZebra20No2/disc_a.pth.tar"
@@ -32,6 +35,7 @@ NEW_CHECKPOINT_DISC_B = "../drive/MyDrive/Checkpoints/HorseToZebra20No2/disc_b.p
 NEW_CHECKPOINT_DISC_AM = "../drive/MyDrive/Checkpoints/HorseToZebra20No2/disc_am.pth.tar"
 NEW_CHECKPOINT_DISC_BM = "../drive/MyDrive/Checkpoints/HorseToZebra20No2/disc_bm.pth.tar"
 
+#Transformations to apply to images
 transforms = A.Compose(
     [
         A.Resize(width=256, height=256),
