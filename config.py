6f8b577c91dc66a1c1990b5c43c769cca2b822d0
Federico6419 <102321945+Federico6419@users.noreply.github.com>
2023-07-08 10:45:34 +0200

    Update config.py

--- config.py
+++ config.py
@@ -14,11 +14,11 @@ LAMBDA_MASK = 0.7
 LAMBDA_CYCLE_MASK = 0.3
 
 #Settings
-NUM_EPOCHS = 5
+NUM_EPOCHS = 25
 LOAD_MODEL = False
 SAVE_MODEL = True
 TRAIN_MODEL = True
-TRANSFORMATION = "VanToPhoto"       
+TRANSFORMATION = "AppleToOrange"       
 TRAIN_MASK = "Squared"
 TEST_MASK = "Random"
 TEST_FOLDER_A = "Ukiyoe15"
@@ -34,7 +34,7 @@ CHECKPOINT_DISC_AM = "../drive/MyDrive/Checkpoints/" + FOLDER + "/disc_am.pth.ta
 CHECKPOINT_DISC_BM = "../drive/MyDrive/Checkpoints/" + FOLDER + "/disc_bm.pth.tar"
 
 #Checkpoints to save
-NEW_FOLDER = "VanToPhotoSquared5"
+NEW_FOLDER = "AppleToOrange25"
 NEW_CHECKPOINT_GEN_A = "../drive/MyDrive/Checkpoints/" + NEW_FOLDER + "/gen_a.pth.tar"     
 NEW_CHECKPOINT_GEN_B= "../drive/MyDrive/Checkpoints/" + NEW_FOLDER + "/gen_b.pth.tar"
 NEW_CHECKPOINT_DISC_A = "../drive/MyDrive/Checkpoints/" + NEW_FOLDER + "/disc_a.pth.tar"
