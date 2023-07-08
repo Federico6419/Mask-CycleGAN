d7a35090f753332b1f3c0e43139a27ec45a3687a
Federico6419 <102321945+Federico6419@users.noreply.github.com>
2023-07-08 15:50:06 +0200

    Update config.py

--- config.py
+++ config.py
@@ -14,18 +14,18 @@ LAMBDA_MASK = 0.7
 LAMBDA_CYCLE_MASK = 0.3
 
 #Settings
-NUM_EPOCHS = 5
+NUM_EPOCHS = 25
 LOAD_MODEL = True
 SAVE_MODEL = True
 TRAIN_MODEL = True
-TRANSFORMATION = "VanToPhoto"       
+TRANSFORMATION = "AppleToOrange"       
 TRAIN_MASK = "Squared"
 TEST_MASK = "Random"
 TEST_FOLDER_A = "Ukiyoe15"
 TEST_FOLDER_B = "PhotoUkiyoe15"
 
 #Checkpoints to load
-FOLDER = "VanToPhotoSquared5"
+FOLDER = "AppleToOrangeSquared25"
 CHECKPOINT_GEN_A = "../drive/MyDrive/Checkpoints/" + FOLDER + "/gen_a.pth.tar"     
 CHECKPOINT_GEN_B= "../drive/MyDrive/Checkpoints/" + FOLDER + "/gen_b.pth.tar"
 CHECKPOINT_DISC_A = "../drive/MyDrive/Checkpoints/" + FOLDER + "/disc_a.pth.tar"
@@ -34,7 +34,7 @@ CHECKPOINT_DISC_AM = "../drive/MyDrive/Checkpoints/" + FOLDER + "/disc_am.pth.ta
 CHECKPOINT_DISC_BM = "../drive/MyDrive/Checkpoints/" + FOLDER + "/disc_bm.pth.tar"
 
 #Checkpoints to save
-NEW_FOLDER = "VanToPhotoSquared10"
+NEW_FOLDER = "AppleToOrangeSquared50"
 NEW_CHECKPOINT_GEN_A = "../drive/MyDrive/Checkpoints/" + NEW_FOLDER + "/gen_a.pth.tar"     
 NEW_CHECKPOINT_GEN_B= "../drive/MyDrive/Checkpoints/" + NEW_FOLDER + "/gen_b.pth.tar"
 NEW_CHECKPOINT_DISC_A = "../drive/MyDrive/Checkpoints/" + NEW_FOLDER + "/disc_a.pth.tar"
