89ccb5abc628d3d37e09a42eb9d8f55cf8894372
Federico6419 <102321945+Federico6419@users.noreply.github.com>
2023-07-18 09:25:42 +0200

    Update discriminator.py

--- discriminator.py
+++ discriminator.py
@@ -2,17 +2,18 @@ import config
 import torch
 import torch.nn as nn
 
-#Define the discriminator model, composed by convolutional blocks
+#Let's define the discriminator model, that is composed by convolutional blocks
 class Discriminator(nn.Module):
     def __init__(self, in_channels=3):
         super().__init__()
 
-        #Function that initializes weights from aGaussian distribution N(0, 0.02)
+        #Function that initializes weights from a Gaussian distribution N(0, 0.02)
         def init_weights(m):
             if type(m) == nn.Conv2d:
                 torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
-            
-        self.initial = nn.Sequential(             #The first block doesn't use instance normalization
+
+        #The first block doesn't use instance normalization
+        self.initial = nn.Sequential(             
             nn.Conv2d(in_channels, 64, kernel_size=4, stride=2, padding=1, padding_mode="reflect"),
             nn.LeakyReLU(0.2, inplace=True),
         ).apply(init_weights)
